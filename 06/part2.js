b354e58cf697a9e5830dc27094485311f2c6a05a
continue47 <contactandrei123@gmail.com>
2022-12-06 10:04:23 +0200

    Day 6 - parts 1 and 2

---
+++ 06/part2.js
@@ -0,0 +1,14 @@
+const input = require("fs").readFileSync("input.txt", "utf8");
+
+let currentMarker = "";
+for (let i = 0; i < input.length; i++) {
+  currentMarker += input[i];
+  if (currentMarker.length > 14) {
+    currentMarker = currentMarker.substr(1);
+    //https://stackoverflow.com/a/37932256
+    if (new Set(currentMarker).size == currentMarker.length) {
+      console.log(i + 1);
+      return;
+    }
+  }
+}
